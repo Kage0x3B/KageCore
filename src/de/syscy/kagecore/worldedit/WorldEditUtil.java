c3e72fd1dd49f3e6f7e2ec064362a8e2c16788d5
Kage0x3B <kage0x3b@outlook.com>
2017-03-02 17:37:28 +0100

    Fixed the WorldEditUtil

--- src/de/syscy/kagecore/worldedit/WorldEditUtil.java
+++ src/de/syscy/kagecore/worldedit/WorldEditUtil.java
@@ -9,6 +9,7 @@ import org.bukkit.plugin.Plugin;
 import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.bukkit.BukkitWorld;
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 import com.sk89q.worldedit.world.World;
 
 import de.syscy.kagecore.KageCore;
@@ -18,16 +19,18 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class WorldEditUtil {
+	private static @Getter WorldEditPlugin worldEditPlugin;
 	private static @Getter WorldEdit worldEdit;
 
 	private static @Getter(value = AccessLevel.PROTECTED) Map<org.bukkit.World, World> worldCache = new HashMap<>();
 
 	protected static boolean initWorldEdit() {
-		if(worldEdit == null) {
-			Plugin worldEditPlugin = Bukkit.getPluginManager().getPlugin("WorldEdit");
+		if(worldEditPlugin == null) {
+			Plugin plugin = Bukkit.getPluginManager().getPlugin("WorldEdit");
 
-			if(worldEditPlugin != null) {
-				worldEdit = (WorldEdit) worldEditPlugin;
+			if(plugin != null) {
+				worldEditPlugin = (WorldEditPlugin) plugin;
+				worldEdit = worldEditPlugin.getWorldEdit();
 			} else {
 				KageCore.debugMessage("WorldEdit is not installed!");
 
