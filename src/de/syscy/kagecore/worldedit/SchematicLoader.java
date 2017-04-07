ceca46782694862a65b60e4fdd8760e49cc8a1f9
Kage0x3B <kage0x3b@outlook.com>
2017-04-07 22:06:22 +0200

    Added utilities for saving schematics and loading them from within the world

--- src/de/syscy/kagecore/worldedit/SchematicLoader.java
+++ src/de/syscy/kagecore/worldedit/SchematicLoader.java
@@ -5,15 +5,24 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import org.bukkit.Location;
 import org.bukkit.World;
 
+import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.MaxChangedBlocksException;
+import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.extent.clipboard.BlockArrayClipboard;
 import com.sk89q.worldedit.extent.clipboard.Clipboard;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardReader;
+import com.sk89q.worldedit.function.operation.ForwardExtentCopy;
+import com.sk89q.worldedit.function.operation.Operations;
+import com.sk89q.worldedit.regions.CuboidRegion;
 import com.sk89q.worldedit.session.ClipboardHolder;
 import com.sk89q.worldedit.util.io.Closer;
 import com.sk89q.worldedit.world.registry.WorldData;
 
+import de.syscy.kagecore.util.BoundingBox;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -57,5 +66,33 @@ public class SchematicLoader {
 		closer.close();
 
 		return new Schematic(WorldEditUtil.createSession(weWorld), new ClipboardHolder(clipboardHolder.getClipboard(), clipboardHolder.getWorldData()));
+	}
+
+	public static Schematic loadArea(World world, BoundingBox boundingBox, Location origin) {
+		if(!WorldEditUtil.initWorldEdit()) {
+			return null;
+		}
+
+		com.sk89q.worldedit.world.World weWorld = WorldEditUtil.getWEWorld(world);
+
+		CuboidRegion region = new CuboidRegion(toVector(boundingBox.getMin()), toVector(boundingBox.getMax()));
+
+		EditSession editSession = WorldEditUtil.createSession(weWorld);
+
+		BlockArrayClipboard clipboard = new BlockArrayClipboard(region);
+		clipboard.setOrigin(toVector(origin));
+		ForwardExtentCopy copy = new ForwardExtentCopy(editSession, region, clipboard, region.getMinimumPoint());
+
+		try {
+			Operations.completeLegacy(copy);
+		} catch(MaxChangedBlocksException ex) {
+			return null;
+		}
+
+		return new Schematic(editSession, new ClipboardHolder(clipboard, editSession.getWorld().getWorldData()));
+	}
+
+	private static Vector toVector(Location location) {
+		return new Vector(location.getBlockX(), location.getBlockY(), location.getBlockZ());
 	}
 }
