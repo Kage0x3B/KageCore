aca9d9666834c25d40632dc43a20118ef41e2c40
Kage0x3B <moritz.hein@live.de>
2021-04-03 21:14:10 +0200

    Try fixing custom entities for 1.16.5

--- src/de/syscy/kagecore/entityregistry/EntityRegistry.java
+++ src/de/syscy/kagecore/entityregistry/EntityRegistry.java
@@ -1,27 +1,30 @@
 package de.syscy.kagecore.entityregistry;
 
-import net.minecraft.server.v1_16_R3.EntityTypes;
-import net.minecraft.server.v1_16_R3.EnumCreatureType;
-import net.minecraft.server.v1_16_R3.World;
-import net.minecraft.server.v1_16_R3.WorldServer;
+import com.mojang.datafixers.DataFixUtils;
+import com.mojang.datafixers.types.Type;
+import de.syscy.kagecore.KageCore;
+import net.minecraft.server.v1_16_R3.*;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_16_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftLivingEntity;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class EntityRegistry {
-    private static Map<String, EntityTypes<?>> registeredEntityMap = new HashMap<>();
+    private static final Map<String, EntityTypes<?>> registeredEntityMap = new HashMap<>();
 
     public static void init() {
 
     }
 
-    public static void registerEntity(String entityName, String replacingName, EntityConstructor entityConstructor, EnumCreatureType creatureType) {
-		/*Map<String, Type<?>> types = (Map<String, Type<?>>) DataConverterRegistry.a().getSchema(DataFixUtils.makeKey(SharedConstants.a().getWorldVersion())).findChoiceType(DataConverterTypes.ENTITY).types();
-		types.put("minecraft:" + entityName, types.get("minecraft:" + replacingName));
-		EntityTypes.a<net.minecraft.server.v1_16_R3.Entity> a = EntityTypes.a.a(entityConstructor, creatureType);
-		registeredEntityMap.put(entityName.toLowerCase(), IRegistry.a(IRegistry.ENTITY_TYPE, entityName, a.a(entityName)));*/
+    public static <T extends Entity> void registerEntity(String entityName, String replacingName, EntityConstructor<T> entityConstructor, EnumCreatureType creatureType) {
+        Map<String, Type<?>> types = (Map<String, Type<?>>) DataConverterRegistry.a().getSchema(DataFixUtils.makeKey(SharedConstants.getGameVersion().getWorldVersion())).findChoiceType(DataConverterTypes.ENTITY).types();
+        types.put("minecraft:" + entityName, types.get("minecraft:" + replacingName));
+
+        EntityTypes.Builder<T> a = EntityTypes.Builder.a(entityConstructor, creatureType);
+        registeredEntityMap.put(entityName.toLowerCase(), IRegistry.a(IRegistry.ENTITY_TYPE, entityName, a.a(entityName)));
     }
 
     public static org.bukkit.entity.Entity spawnEntity(String entityName, Location location) {
@@ -32,29 +35,27 @@ public class EntityRegistry {
         if (type == null) {
             throw new IllegalArgumentException("Invalid custom entity name \"" + entityName + "\"! Already registered the entity?");
         }
-/*
-		net.minecraft.server.v1_16_R3.Entity nmsEntity = type.b(world, null, null, null, new BlockPosition(location.getX(), location.getY(), location.getZ()), EnumMobSpawn.MOB_SUMMONED, false, false);
 
-		if(nmsEntity != null) {
-			nmsEntity.setPositionRotation(location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
+        net.minecraft.server.v1_16_R3.Entity nmsEntity = type.createCreature(world, null, null, null, new BlockPosition(location.getX(), location.getY(), location.getZ()), EnumMobSpawn.MOB_SUMMONED, false, false);
 
-			world.addEntity(nmsEntity, CreatureSpawnEvent.SpawnReason.CUSTOM);
+        if (nmsEntity != null) {
+            nmsEntity.setPositionRotation(location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
 
-			if(nmsEntity instanceof EntityLiving) {
-				((CraftLivingEntity) nmsEntity.getBukkitEntity()).setRemoveWhenFarAway(false);
-			}
+            world.addEntity(nmsEntity, CreatureSpawnEvent.SpawnReason.CUSTOM);
 
-			return nmsEntity.getBukkitEntity();
-		} else {
-			KageCore.debugMessage("Could not spawn custom entity \"" + entityName + "\".");
+            if (nmsEntity instanceof EntityLiving) {
+                ((CraftLivingEntity) nmsEntity.getBukkitEntity()).setRemoveWhenFarAway(false);
+            }
 
-			return null;
-		}*/
+            return nmsEntity.getBukkitEntity();
+        } else {
+            KageCore.debugMessage("Could not spawn custom entity \"" + entityName + "\".");
 
-        return null;
+            return null;
+        }
     }
 
-    public interface EntityConstructor<T extends net.minecraft.server.v1_16_R3.Entity> extends EntityTypes.b<T> {
+    public interface EntityConstructor<T extends Entity> extends EntityTypes.b<T> {
         T create(EntityTypes<T> type, World world);
     }
 }
