8ce3933dc01b1c5ef806c35d9f3c1b44318ad3d5
Kage0x3B <moritz.hein@live.de>
2021-04-08 21:27:06 +0200

    Update experience util for 1.16.5, setting level to -1 wasn't valid anymore

--- src/de/syscy/kagecore/util/ExperienceUtil.java
+++ src/de/syscy/kagecore/util/ExperienceUtil.java
@@ -6,11 +6,11 @@ public class ExperienceUtil {
 	public static void setTotalExperience(Player player, int exp) {
 		clearExp(player);
 
-		player.giveExp(exp);
+		player.giveExp(Math.min(exp, 0));
 	}
 
 	public static void clearExp(Player player) {
-		player.setLevel(-1);
+		player.setLevel(0);
 		player.setExp(0);
 		player.setTotalExperience(0);
 	}
